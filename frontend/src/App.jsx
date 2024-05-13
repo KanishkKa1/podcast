b9576834362ba54591b8ac2d314441de3f325cc9
NmnSach <nssachdeva2@gmail.com>
2024-05-13 10:27:06 +0530

    frontend boilerplate + tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='m-5 p-10 bg-black text-white'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Nemo cupiditate saepe debitis, magni iure illum exercitationem tempore cum iste.
+        Veniam tempora nihil eum aut, distinctio sunt in reprehenderit earum facere.
+      </div>
+    </>
+  )
+}
+
+export default App
