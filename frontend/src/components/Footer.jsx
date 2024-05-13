e5bc8a527b2094edc9f2148ef4dbc1c522bc9d18
NmnSach <nssachdeva2@gmail.com>
2024-05-13 21:30:56 +0530

    added footer file - incomplete

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div
+            className="bg-cover bg-center"
+            style={{ backgroundImage: "url('/')" }}
+        >
+
+            <div>
+                <h1>h2h@gmail.com</h1>
+                <p>Copyright © Here To Hear 2024</p>
+            </div>
+
+
+
+        </div>
+
+    )
+}
+
+export default Footer
