e288e92dc52e62c6251e468aa29fe6869497d0c5
NmnSach <nssachdeva2@gmail.com>
2024-05-14 21:20:54 +0530

    Signup to Login link

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -27,8 +28,8 @@ const Signup = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+    <div className="min-h-screen flex items-center justify-center bg-gray-200 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 bg-white p-10 shadow-sm">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up for an account</h2>
         </div>
@@ -59,12 +60,17 @@ const Signup = () => {
           />
           <button
             type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4"
           >
             Sign up
           </button>
         </form>
+
+        <div className="flex justify-center items-center">
+          <p className="text-lg">Already have an account? <Link to='/login' className="font-bold underline">Login</Link></p>
+        </div>
       </div>
+
     </div>
   );
 };
