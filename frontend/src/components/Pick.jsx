9c9401ece5c921b61a4c8565d0c07b146ffaea19
NmnSach <nssachdeva2@gmail.com>
2024-05-15 00:35:48 +0530

    Linked entire tile in pick section

--- frontend/src/components/Pick.jsx
+++ frontend/src/components/Pick.jsx
@@ -8,31 +8,36 @@ const Pick = () => {
                 <h1 className='text-4xl font-serif my-10'>Are you here to tell a story or listen to one?</h1>
             </div>
             <div className='flex justify-center items-center w-8/12 h-96'>
-                <div
-                    className='flex justify-center items-center rounded-xl w-full h-full mx-5 hover:shadow-xl hover:shadow-black'
-                    style={{
-                        backgroundImage: "url('/speaker.jpg')",
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    }}
-                >
-                    <Link to='/speaker' className='item'><span className='text-4xl text-white font-bold'>Speaker</span></Link>
 
-                </div>
+                <Link to='/speaker' className='item w-full h-full mx-5'>
+                    <div
+                        className='flex justify-center items-center rounded-xl w-full h-full hover:shadow-xl hover:shadow-black'
+                        style={{
+                            backgroundImage: "url('/speaker.jpg')",
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    >
+                        <span className='text-4xl text-white font-bold'>Speaker</span>
 
-                <div
-                    className='flex justify-center items-center rounded-xl w-full h-full mx-5 hover:shadow-xl hover:shadow-black'
-                    style={{
-                        backgroundImage: "url('/listener.jpg')",
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    }}
-                >
-                    <Link to='/listener' className='item'><span className='text-4xl text-white font-bold'>Listener</span></Link>
+                    </div>
+                </Link>
 
-                </div>
+                <Link to='/listener' className='item w-full h-full mx-5'>
+                    <div
+                        className='flex justify-center items-center w-full h-full rounded-xl hover:shadow-xl hover:shadow-black'
+                        style={{
+                            backgroundImage: "url('/listener.jpg')",
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    >
+                        <span className='text-4xl text-white font-bold'>Listener</span>
+
+                    </div>
+                </Link>
             </div>
-        </div>
+        </div >
     );
 };
 
