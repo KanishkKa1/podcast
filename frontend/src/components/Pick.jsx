2bef30fcc071de18bbf82c65a7a9c0860721b1d8
NmnSach <nssachdeva2@gmail.com>
2024-05-14 15:22:07 +0530

    /pick complete

--- frontend/src/components/Pick.jsx
+++ frontend/src/components/Pick.jsx
@@ -7,14 +7,28 @@ const Pick = () => {
             <div className='flex justify-center items-center'>
                 <h1 className='text-4xl font-serif my-10'>Are you here to tell a story or listen to one?</h1>
             </div>
-            <div className='flex justify-center items-center w-[600px] h-[450px] shadow-xl'>
-                <div>
-                    <Link to='/speaker' className='item'><span>Speaker</span></Link>
+            <div className='flex justify-center items-center w-8/12 h-96'>
+                <div
+                    className='flex justify-center items-center rounded-xl w-full h-full mx-5 hover:shadow-xl hover:shadow-black'
+                    style={{
+                        backgroundImage: "url('/speaker.jpg')",
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                >
+                    <Link to='/speaker' className='item'><span className='text-4xl text-white font-bold'>Speaker</span></Link>
 
                 </div>
 
-                <div>
-                    <Link to='/listener' className='item'><span>Listener</span></Link>
+                <div
+                    className='flex justify-center items-center rounded-xl w-full h-full mx-5 hover:shadow-xl hover:shadow-black'
+                    style={{
+                        backgroundImage: "url('/listener.jpg')",
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                >
+                    <Link to='/listener' className='item'><span className='text-4xl text-white font-bold'>Listener</span></Link>
 
                 </div>
             </div>
